8a3f44e0a8b9652ac9a13b926205c775cd7bfaec
aliprentice <57479066+aliprentice@users.noreply.github.com>
2025-04-03 09:27:35 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Next.js with MongoDB on Render!</h1>
+      <p>This is a simple cloud-based deployment.</p>
+    </div>
+  );
+}
